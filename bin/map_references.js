af30e3187d2b8e93f1e66a1c787524afb5da4405
Caleb Maclennan <caleb@alerque.com>
2016-01-07 16:51:28 +0200

    Implement proof-of-concept for parsing references out of posts

--- bin/map_references.js
+++ bin/map_references.js
@@ -4,15 +4,16 @@ var xml_file = process.argv[1];
 
 // TODO: parse XML file and iterate through the posts instead of this dummy array
 var posts = [
-  { post_id: 0, body: "test Romans 10:4 stuff" },
-  { post_id: 0, body: "test Revalation 7:6-10 stuff" },
-  { post_id: 0, body: "test John 3:16 stuff Romans 3:23 more stuff" }
+  { id: 0, body: "test Romans 10:4 stuff" },
+  { id: 1, body: "test Revalation 7:6-10 stuff" },
+  { id: 2, body: "test John 3:16 stuff Romans 3:23 more stuff" }
 ];
 
 var bcv_parser = require("bible-passage-reference-parser/js/en_bcv_parser").bcv_parser;
 var bcv = new bcv_parser;
 
 posts.forEach(function(post) {
-  console.log(post);
+  var p = bcv.parse(post.body);
+  console.log(post.id, p.osis());
 });
 
