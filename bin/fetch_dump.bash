d46ad5359580e9f4a98b0a06a45bc623ed448620
Caleb Maclennan <caleb@alerque.com>
2016-01-11 10:16:49 +0200

    Normalize curl usage with short flags

--- bin/fetch_dump.bash
+++ bin/fetch_dump.bash
@@ -11,8 +11,7 @@ case $1 in
     [a-z]*)
         archive_url="https://archive.org/download/stackexchange/${1}.7z"
         dump_url=$(curl -w "%{url_effective}" -I -L -s -S $archive_url -o /dev/null)
-        curl -o /dev/null -s --fail --head $dump_url ||
-            flunk "Dump file for site ${1} not found"
+        curl -o /dev/null -s -f -I $dump_url || flunk "Dump file for site ${1} not found"
         ;;
     *)
         flunk "Not a valid site: ${1}"
@@ -20,4 +19,4 @@ case $1 in
         ;;
 esac
 
-curl -o "${1}.7z" -C - --progress $dump_url
+curl -o "data/${1}.7z" -C - -# $dump_url
