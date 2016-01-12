69fceda9d8bc04d029e2fab1e0f174581717ca99
Caleb Maclennan <caleb@alerque.com>
2016-01-12 17:10:41 +0200

    Start checking for site specific settings
    See #10

--- bin/search.js
+++ bin/search.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var searcher = require( '../src/search.js' );
 var site = process.argv[2];
 var query = process.argv[3];
-var index = require( '../data/' + site + '-index.json' );
 var config = require( '../config.json' );
+var searcher = require( '../src/search.js' );
+var index = require( '../data/' + site + '-index.json' );
 
 var results = searcher( query, index ).forEach( function( post ) {
   var url = "http://" + site + ".stackexchange.com/" + post.type + "/" + post.id + '/' + config.sites[site].referral;
--- src/bcv_parser.js
+++ src/bcv_parser.js
@@ -1,9 +1,20 @@
 var bcv_parser = require( 'bible-passage-reference-parser/js/en_bcv_parser' );
 var config = require( '../config.json' );
 
+var opts = config;
+if ( typeof site === 'string' ) {
+  opts.include_apocrypha = config.sites[site].include_apocrypha;
+  opts.include_chapters = config.sites[site].include_chapters;
+  if ( typeof config.sites[site].bcv_options !== 'undefined' ) {
+    for ( var attr in config.sites[site].bcv_options ) {
+      opts.bcv_options[attr] = config.sites[site].bcv_options[attr];
+    }
+  }
+}
+
 // Our standard bcv parser;
 var bcv = new bcv_parser.bcv_parser;
-bcv.include_apocrypha( config.include_apocrypha );
-bcv.set_options( config.bcv_options );
+bcv.include_apocrypha( opts.include_apocrypha );
+bcv.set_options( opts.bcv_options );
 
 module.exports.bcv = bcv;
