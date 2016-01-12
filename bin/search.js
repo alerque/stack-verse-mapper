735ea2e92720691a95a969b75d0a2e84d810a850
Caleb Maclennan <caleb@alerque.com>
2016-01-12 16:35:17 +0200

    Output full URLs to the console

--- bin/search.js
+++ bin/search.js
@@ -4,5 +4,9 @@ var searcher = require( '../src/search.js' );
 var site = process.argv[2];
 var query = process.argv[3];
 var index = require( '../data/' + site + '-index.json' );
+var config = require( '../config.json' );
 
-console.log( searcher( query, index ) );
+var results = searcher( query, index ).forEach( function( post ) {
+  var url = "http://" + site + ".stackexchange.com/" + post.type + "/" + post.id + '/' + config.sites[site].referral;
+  console.log( url, post );
+});
