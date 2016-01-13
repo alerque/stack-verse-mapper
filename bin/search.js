f996316e530a6500f08835724f97aa5f8f07e80f
Dannii Willis <curiousdannii@gmail.com>
2016-01-13 11:40:51 +1000

    Update bin/search.js

--- bin/search.js
+++ bin/search.js
@@ -3,10 +3,10 @@
 var site = process.argv[2];
 var query = process.argv[3];
 var config = require( '../config.json' );
-var searcher = require( '../src/search.js' );
+var search = require( '../src/search.js' );
 var index = require( '../data/' + site + '-index.json' );
 
-var results = searcher( query, index ).forEach( function( post ) {
+var results = search.search( query, index ).forEach( function( post ) {
   var url = "http://" + site + ".stackexchange.com/" + post.type + "/" + post.id + '/' + config.sites[site].referral;
-  console.log( url, post );
+  console.log( url, post.type.toUpperCase() + ':', post.title );
 });
